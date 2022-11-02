4d629e94b685c7ef162f9872b7e2bc7860833ec8
jj <01158857@pw.edu.pl>
2022-11-02 21:27:33 +0100

    Basic movement

--- Snake-toss/src/App.jsx
+++ Snake-toss/src/App.jsx
@@ -6,8 +6,9 @@ import Pixel from './Components/Pixel'
 function App() {
   const [gridInfo, setGridInfo] = React.useState([])
   const [switchChoice, setSwitchChoice] = React.useState(true)
-  const [takenSqaures, setTakenSqares] = React.useState([-1,-1,-1,-1])
+  const [headSquares, setHeadSquares] = React.useState([-1,-1,-1,-1])
   const [chosePositionState, setChosePositionState] = React.useState(true)
+  const [color, setColor] = React.useState(true)
 
 React.useEffect(function(){ 
     const newGridInfo = []
@@ -40,10 +41,10 @@ function changePixel (column, row){
 
   if(!chosePositionState){return}
 
-  if(takenSqaures[0]===column && takenSqaures[1]===row){return}
-  if(takenSqaures[2]===column && takenSqaures[3]===row){return}
+  if(headSquares[0]===column && headSquares[1]===row){return}
+  if(headSquares[2]===column && headSquares[3]===row){return}
 
-  setTakenSqares(prevTable => {
+  setHeadSquares(prevTable => {
     const newtable=prevTable
     if(switchChoice){
       newtable[0]=column
@@ -90,14 +91,85 @@ function changePixel (column, row){
       }
       }
     }
-    console.log(newInfo)
     return newInfo
   })
   setSwitchChoice(prevState => !prevState)
 }
 
-function startGame(){
+function startGameCheck(){
+  for(let i=0; i<headSquares.length; i++){
+    if(headSquares[i]===-1){
+      setChosePositionState(true)
+      return
+    } 
+  }
   setChosePositionState(false)
+}
+
+function move(column,row,state,move){
+  setGridInfo(prevInfo => {
+    const position=column+10*row
+    const head=state+2
+    const info=[]
+    for(let i = 0; i <prevInfo.length; i++){
+      const currentInfo = prevInfo[i]
+      
+      if(i === position-move){
+        const updatedInfo = {
+          ...currentInfo, state:head
+        }
+        info.push(updatedInfo)
+      }
+      else if(i === position){
+        
+        const updatedInfo = {
+          ...currentInfo, state:state
+        }
+        info.push(updatedInfo)
+      } else{
+        const updatedInfo = currentInfo
+
+        info.push(updatedInfo)  
+      }
+    }
+    console.log(info)
+    return info
+  })
+}
+
+function goUp(column, row, state, color){
+  move(column,row,state,10)
+  return [column, row-1]
+}
+
+function goDown(column, row, state){
+  move(column,row,state,-10)
+  return [column,row+1]
+}
+
+function goLeft(column, row, state){
+  move(column,row,state,1)
+  return [column-1,row]
+}
+
+function goRight(column, row, state){
+  move(column,row,state,-1)
+  return [column+1,row]
+}
+
+function startGame(){
+  let blueHead=[headSquares[0],headSquares[1]]
+  let orangeHead=[headSquares[2],headSquares[3]]
+  startGameCheck()
+  blueHead = goUp(blueHead[0],blueHead[1],1)
+  blueHead = goUp(blueHead[0],blueHead[1],1)
+  blueHead = goLeft(blueHead[0],blueHead[1],1)
+  blueHead = goLeft(blueHead[0],blueHead[1],1)
+  blueHead = goDown(blueHead[0],blueHead[1],1)
+  blueHead = goDown(blueHead[0],blueHead[1],1)
+  blueHead = goDown(blueHead[0],blueHead[1],1)
+  blueHead = goRight(blueHead[0],blueHead[1],1)
+  console.log(gridInfo)
 }
 
   return (
@@ -112,22 +184,24 @@ function startGame(){
       <p>now chosing:</p>
 
       <Pixel
-      key={"special"}
-      column={1000}
-      row={1000}
-      state={switchChoice ? 3 : 4}
+        key={"special"}
+        column={1000}
+        row={1000}
+        state={switchChoice ? 3 : 4}
       />
+
+        <button onClick={startGame}>start</button>
 
       </>
 
         :
-        
+
         <>
           <p>game started</p>
         </>
 
         }
-      <button onClick={startGame}>start</button>
+      
     </div>
   )
 }
